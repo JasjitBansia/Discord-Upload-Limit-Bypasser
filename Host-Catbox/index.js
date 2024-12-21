db2432b7683677507913d44d016f177fda509a7d
Jasjit Singh <jasjitbansia@gmail.com>
2024-12-21 18:21:22 +0530

    Copy function

--- Host-Catbox/index.js
+++ Host-Catbox/index.js
@@ -1,3 +1,4 @@
+const { copyURL } = require("../Functions/copyURL.js");
 const { validatePath } = require("../Functions/pathValidation.js");
 const { Catbox } = require("catbox.moe");
 let catbox = new Catbox();
@@ -7,7 +8,9 @@ async function main() {
   console.log("Uploading...");
   const response = await catbox.upload(validatedPath);
   if (response.startsWith("https://files.catbox.moe")) {
-    console.log(`Here's your url: [video](https://embeds.video/${response})`);
+    let text = `[video](https://embeds.video/${response})`;
+    copyURL(text);
+    console.log(`Here's your url (copied): ${text}`);
   } else {
     console.log(response);
     main();
--- Host-Fileditch/index.js
+++ Host-Fileditch/index.js
@@ -1,5 +1,6 @@
 const { validatePath } = require("../Functions/pathValidation.js");
 const { exec } = require("child_process");
+const { copyURL } = require("../Functions/copyURL.js");
 setInterval(() => {}, 1000);
 async function main() {
   let validatedPath = await validatePath();
@@ -10,9 +11,9 @@ async function main() {
       if (res) {
         let json = JSON.parse(res.substring(res.indexOf("{")));
         if (json.success === true) {
-          console.log(
-            `Here's your url: [video](https://embeds.video/${json.files[0].url})`
-          );
+          let text = `[video](https://embeds.video/${json.files[0].url})`;
+          copyURL(text);
+          console.log(`Here's your url (copied): ${text}`);
         }
       } else {
         main();
