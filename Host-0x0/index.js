3fbf265c71ef57ac6b20fc2c27263bb70ce4157b
Jasjit Singh <jasjitbansia@gmail.com>
2025-01-04 18:23:03 +0530

    0x0 host

---
+++ Host-0x0/index.js
@@ -0,0 +1,18 @@
+const { validatePath } = require("../Functions/pathValidation.js");
+const { exec } = require("child_process");
+const { copyURL } = require("../Functions/copyURL.js");
+setInterval(() => {}, 1000);
+async function main() {
+  let validatedPath = await validatePath();
+  console.log("Uploading...");
+  exec(`curl.exe -F 'file=@${validatedPath}' https://0x0.st`, (err, res) => {
+    if (res.startsWith("https://0x0.st/")) {
+      let text = `[video](${res.trim()})`;
+      copyURL(text);
+      console.log(`Here's your url (copied): ${text}`);
+    } else {
+      main();
+    }
+  });
+}
+main();
