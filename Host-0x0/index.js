fe3dbf625c5c29a1411bc66af59c01ca95e81528
Jasjit Singh <jasjitbansia@gmail.com>
2025-02-14 21:08:00 +0530

    minor fix

--- Host-0x0/index.js
+++ Host-0x0/index.js
@@ -5,7 +5,11 @@ setInterval(() => {}, 1000);
 async function main() {
   let validatedPath = await validatePath();
   console.log("Uploading...");
-  exec(`curl -F 'file=@${validatedPath}' https://0x0.st`, (err, res) => {
+  exec(`curl -F file=@${validatedPath} https://0x0.st`, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
     if (res.startsWith("https://0x0.st/")) {
       let text = `[video](https://embeds.video/${res.trim()})`;
       copyURL(text);
